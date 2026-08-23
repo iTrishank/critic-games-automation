4e3f4f6df658ebe3abe75b82271741e000ffb36e
iTrishank <trishank9999@gmail.com>
2026-08-23 19:25:46 +0300

    Configure Chromium for Railway

--- src/entities/game/api/metacritic.scraper.ts
+++ src/entities/game/api/metacritic.scraper.ts
@@ -441,6 +441,12 @@ async function scrapeUserReviews(
 export async function createMetacriticBrowser(): Promise<Browser> {
   return chromium.launch({
     headless: true,
+    args: [
+      "--no-sandbox",
+      "--disable-setuid-sandbox",
+      "--disable-dev-shm-usage",
+      "--disable-gpu",
+    ],
   });
 }
 
